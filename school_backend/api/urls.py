330ae3fcdf2adf0442e41649f1fcafec7140c822
Nikita <zaebumba@gmail.com>
2021-01-26 17:12:40 +0700

    Add path for mongo_logs

--- school_backend/api/urls.py
+++ school_backend/api/urls.py
@@ -1,16 +1,18 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import SchoolViewSet, StudentViewSet, user_registration
+from .views import SchoolViewSet, StudentViewSet, UserList, mongo_logs
 
 router = DefaultRouter()
 
-router.register('students', StudentViewSet, basename='student')
-router.register('schools', SchoolViewSet, basename='school')
+router.register('students', StudentViewSet, basename='students')
+router.register('schools', SchoolViewSet, basename='schools')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('register/', user_registration),
+    path('logs/', mongo_logs),
+    path('logs/<str:model>/', mongo_logs),
+    path('register/', UserList.as_view()),
     path('login/', include('rest_framework.urls', namespace='rest_framework'))
 ]
