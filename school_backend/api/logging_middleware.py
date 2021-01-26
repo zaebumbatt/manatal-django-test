2fe428cbe8f871be22897d69e3c1cfac3cb9be3c
Nikita <zaebumba@gmail.com>
2021-01-26 17:09:37 +0700

    Add logging to MongoDB

--- school_backend/api/logging_middleware.py
+++ school_backend/api/logging_middleware.py
@@ -1,16 +1,54 @@
-from .mongodb_connector import col
+from datetime import datetime
+
+from .mongodb_connector import schools, students, users
 
 
 class LoggingMiddleware:
+    actions = {
+        'GET': 'List',
+        'POST': 'Create',
+        'PATCH': 'Update',
+        'DELETE': 'Destroy'
+    }
+    models = {
+        'register': 'User',
+        'students': 'Student',
+        'schools': 'School'
+    }
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
+
         if not request.user.username:
             username = 'AnonymousUser'
         else:
             username = request.user.username
-        col.insert_one({username: request.method})
+
+        url = request.get_raw_uri().split('/')[-2]
+        logs = request.get_raw_uri().split('/')[-3]
+        date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        col = None
+
+        if url == 'register' and request.method == 'POST':
+            col = users
+        elif url == 'students' and logs != 'logs':
+            col = students
+        elif url == 'schools' and logs != 'logs':
+            col = schools
+
+        if col:
+            col.insert_one(
+                {
+                    date_time: {
+                        'model': self.models[url],
+                        'username': username,
+                        'action': self.actions[request.method],
+                        'status_code': response.status_code,
+                        'data': response.data
+                    }
+                }
+            )
         return response
